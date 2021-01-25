e2d9b5c8a94bc87c62d797ce7c9f693230c55b4f
Greg Bird <greg.bird.oz@gmail.com>
2021-01-25 15:51:24 +1100

    Formatted for readability

--- GB-clean-MD.py
+++ GB-clean-MD.py
@@ -9,14 +9,14 @@ class CleanMd(sublime_plugin.TextCommand):
         self.view.set_status('cleaning',status_msg)
 
         substitutions = [
-        ('[·•] +', '* '),                    # Replace bullets with *
-        ('^\\\(\d\.) +', '\\1 '),            # Replace \1. etc with 1.
-        ('#+\n', ''),                       # Empty headings
-        ('((# |\*|\d\.)) {2,}', '\\1 '),     # Blank spaces before headings, bulets, numbers
-        ('# +\*\*(.*?)\*\*', ' \\1'),         # Bolded headings
-        ('(^\* .*)\n{2,}(?=\*)', '\\1\n'),   # Collapse lines b/n bullets
-        ('(^\d\. .*)\n{2,}(?=\d)', '\\1\n'), # Collapse lines b/n numbered lists
-        ('\n{3,}', '\n\n')                   # Multiple empty lines
+            ('[·•] +', '* '),                    # Replace decorative bullets with *
+            ('^\\\(\d\.) +', '\\1 '),            # Replace escaped \1. etc with 1.
+            ('#+\n', ''),                        # Remove empty headings
+            ('((# |\*|\d\.)) {2,}', '\\1 '),     # Blank spaces before headings, bullets, numbers
+            ('# +\*\*(.*?)\*\*', ' \\1'),        # Un-bold headings
+            ('(^\* .*)\n{2,}(?=\*)', '\\1\n'),   # Collapse lines b/n bullets
+            ('(^\d\. .*)\n{2,}(?=\d)', '\\1\n'), # Collapse lines b/n numbered lists
+            ('\n{3,}', '\n\n')                   # Collapse multiple empty lines.  Do this last
         ]
 
         replacestrings(self, edit, type, substitutions)
@@ -39,6 +39,7 @@ def replacestrings(self, edit, type, substitutions):
     # Add Bootstrap support
     bootstrap = '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">\n<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>\n<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>\n<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>\n<link href="https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">\n\n'
 
+    # Only add Bootstrap if it has not already been called
     if "bootstrapcdn" not in string:
         string = bootstrap + string
 
